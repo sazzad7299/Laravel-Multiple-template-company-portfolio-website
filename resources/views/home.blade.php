815d5e9df04413a0614a3f03621a7cc9800a1786
sazzad7299 <sazzadurrahman580@gmail.com>
2024-01-18 12:10:35 +0600

    Admin Mastering COmpleted

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('backend.layouts.app')
 
-@section('content')
+@section('backendContent')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
