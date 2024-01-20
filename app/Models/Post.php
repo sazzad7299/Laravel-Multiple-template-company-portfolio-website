1a6a12e76cbcc4f5035cfbc6cf42a16113a39af1
sazzad7299 <sazzadurrahman580@gmail.com>
2024-01-20 12:12:02 +0600

    Post methods created

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\CreatedUpdatedBy;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory,CreatedUpdatedBy,SoftDeletes;
+    protected $table = 'posts';
+
+   protected $fillable = ['title','description','feature_image'];
+    public function scopeActive($query)
+    {
+        $query->where('status', 1);
+    }
+
+    public function scopeSearch($query, $request)
+    {
+            return $query->where('title', 'LIKE', '%'.$request.'%')
+            ->orWhere('desciption', 'LIKE', '%'.$request.'%');
+    }
+
+}
---
+++ database/migrations/2024_01_20_055922_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->date('date');
+            $table->tinyText('post_type')->nullable();
+            $table->string('feature_image');
+            $table->tinyInteger('status')->default(1);
+            $table->foreignId('created_by')->nullable()
+            ->constrained('users', 'id')
+            ->onDelete('set null');
+        $table->foreignId('updated_by')->nullable()
+            ->constrained('users', 'id')
+            ->onDelete('set null');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
